9cefa1719f213e223d085a5320bf844500d58664
Your Name <you@example.com>
2022-12-13 09:01:21 -0300

    commit - v2.0 / duracao de jogo

--- pagamarreco_v2.0/index.js
+++ pagamarreco_v2.0/index.js
@@ -6,15 +6,27 @@ function calcular(){
     var res = document.querySelector('div#res')
     var out = document.querySelector('div#out')
     var fin = document.querySelector('div#fin')
+    var tempojogo = document.getElementsByName('tempojogo')
+    var minutagem = Number('')
+    if (tempojogo[0].checked){
+        minutagem = 60
+    } else if (tempojogo[1].checked){
+        minutagem = 90
+    } else if (tempojogo[2].checked){
+        minutagem = 120
+    } else {
+        alert('Necessário selecionar o tempo do jogo')
+    }
     var nome1 = String(nome.value)
     var minutos1 = Number(minutos.value)
     var valor1 = Number(valor.value)
     var pessoas1 = Number(pessoas.value)
+    var minutosjogo = minutagem
     if (minutos1 > 60){
         alert('ERRO - O VALOR DE MINUTOS DEVE SER INFERIOR A 60')
     } else {
         var quadrapessoa = valor1/pessoas1
-        var atraso = (quadrapessoa/60) * minutos1
+        var atraso = (quadrapessoa/minutosjogo) * minutos1
         var desconto = quadrapessoa - (atraso/(pessoas1-1))
         var total = quadrapessoa + atraso
         var pagoaquadra = desconto * (pessoas1 - 1) + total 
@@ -28,5 +40,6 @@ function calcular(){
         res.innerHTML = `O ${nome1}, se atrasou ${minutos1} minutos, e terá que pagar ${atraso.toFixed(2)} à mais. Totalizando a parte dele em ${total.toFixed(2)}`
         out.innerHTML = `Sendo assim, os outros participantes terão que pagar ${desconto.toFixed(2)} `
         fin.innerHTML = `O valor pago a quadra foi de ${pagoaquadra.toFixed(2)}`
+        //pagoaquadra.toFixed(2)
     }
 }   
