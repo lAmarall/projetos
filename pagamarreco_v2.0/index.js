c35e604e52365c5ec7789eb7f9b9fe83d75d4fcc
Your Name <you@example.com>
2023-01-10 15:30:39 -0300

    commit - CSS Basico

--- pagamarreco_v2.0/index.js
+++ pagamarreco_v2.0/index.js
@@ -22,8 +22,9 @@ function calcular(){
     var valor1 = Number(valor.value)
     var pessoas1 = Number(pessoas.value)
     var minutosjogo = minutagem
-    if (minutos1 > minutos){
-        alert('ERRO - O VALOR DE MINUTOS DEVE SER INFERIOR A 60')
+    if (minutos1 > minutagem){
+        alert(`ERRO - O VALOR DE MINUTOS DEVE SER INFERIOR A ${minutagem} MINUTOS! ESSE ATRASO DEVE SER CONSIDERADO FALTA`)
+        //alert(`ESSE ATRASO DEVE SER CONSIDERADO FALTA!`)
     } else {
         var quadrapessoa = valor1/pessoas1
         var atraso = (quadrapessoa/minutosjogo) * minutos1
@@ -37,9 +38,13 @@ function calcular(){
             res.innerHTML = `O ${nome1}, se atrasou ${minutos1} minutos, e terá que pagar ${atraso.toFixed(2)} à mais. Totalizando a parte dele em ${arredondamento.toFixed(2)}`
             fin.innerHTML = `O valor pago a quadra foi de ${pagoaquadra.toFixed(2)}`
     } */
-        res.innerHTML = `O ${nome1}, se atrasou ${minutos1} minutos, e terá que pagar ${atraso.toFixed(2)} à mais. Totalizando a parte dele em ${total.toFixed(2)}`
-        out.innerHTML = `Sendo assim, os outros participantes terão que pagar ${desconto.toFixed(2)} `
-        fin.innerHTML = `O valor pago a quadra foi de ${pagoaquadra.toFixed(2)}`
+        res.innerHTML = `${nome1}, se atrasou ${minutos1} minutos, e terá que pagar R$${atraso.toFixed(2)} à mais. Totalizando a parte dele em R$${total.toFixed(2)}`
+        out.innerHTML = `Sendo assim, os outros participantes terão que pagar R$${desconto.toFixed(2)} `
+        fin.innerHTML = `O valor pago a quadra foi de R$${pagoaquadra.toFixed(2)}`
         //pagoaquadra.toFixed(2)
     }
 }   
+/*function adicionar(){
+    var elemento = document.getElementById('txtnome2')
+    elemento.className = "show";
+}*/
